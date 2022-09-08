9041262d0a9312bf396f6f88eaa5a6689f6c06b5
Non-PlayerC <1700969761@qq.com>
2022-09-08 18:37:57 +0800

    后面都不是很理解了 qaq

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,15 +1,25 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Search from "./components/search/search";
+import CurrentWeather from "./components/current-weather/current-weather";
+import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
 
 function App() {
   const handleOnSearchChange = (searchDtata) => {
-    console.log(searchDtata);
+    const [lat, lon] = searchDtata.value.split(" ");
+
+    const currentWeather = fetch(
+      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
+    );
+    const forecastFetch = fetch(
+      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
+    );
   };
 
   return (
     <div className="container">
       <Search onSearchChange={handleOnSearchChange} />
+      <CurrentWeather />
     </div>
   );
 }
